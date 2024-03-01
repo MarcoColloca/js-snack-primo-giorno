c8b0c679290136e552d7c452e3c093e8a9a41395
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-01 14:10:36 +0100

    Feat → addex const PI to JS
    PI = 3.14 (number)

--- js/app.js
+++ js/app.js
@@ -4,6 +4,22 @@ console.log("JS Trial")
 //// Esercizio 1
 /// Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
 
-let number = 100;
+let number = 100; //Number
 
-console.log(number);
+console.log(number); //Number
+
+
+
+
+
+
+//// Esercizio 2
+/// Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+
+const PI = 3.14; //Number
+
+console.log(PI); //Number
+
+
+
+
