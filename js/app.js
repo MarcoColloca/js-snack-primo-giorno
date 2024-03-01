4112acd2a86593cbf2e785bfa4173a7a2c57c0c0
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-01 14:06:30 +0100

    Feat → added 1st Java Exercise

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log("JS Trial")
+
+
+//// Esercizio 1
+/// Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
+
+const number = 100;
+
+console.log(number);
