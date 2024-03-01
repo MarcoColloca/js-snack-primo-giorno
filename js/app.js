9db1bbeb34cd5f9a46a98d8f7f3765cf05e6da50
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-01 14:08:08 +0100

    Fix → changed 1st ex const into let

--- js/app.js
+++ js/app.js
@@ -4,6 +4,6 @@ console.log("JS Trial")
 //// Esercizio 1
 /// Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
 
-const number = 100;
+let number = 100;
 
 console.log(number);
